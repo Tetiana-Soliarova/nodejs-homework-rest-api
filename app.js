7b0e432f266d32723949c2c4cad9fa56bb02bdb6
Tetiana-Soliarova <t.s.skorpi1991@ukr.net>
2021-05-22 21:18:36 +0300

    test start

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(helmet())
 app.use(express.static('public'))
-app.use(logger(formatsLogger))
+app.get('env') !== "test" && app.use(logger(formatsLogger))
 app.use(cors({
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
