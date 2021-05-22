a143faba7a0ddee3deafd453ad413642b7ee4ddc
Tetiana-Soliarova <t.s.skorpi1991@ukr.net>
2021-05-22 21:39:15 +0300

    app

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const boolParser = require('express-query-boolean')
 const contactsRouter = require('./routes/contacts')
 const usersRouter = require('./routes/users')
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-app.use(helmet())
-app.use(express.static('public'))
-app.get('env') !== "test" && app.use(logger(formatsLogger))
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use(helmet());
+app.use(express.static('public'));
+app.get('env') !== "test" && app.use(logger(formatsLogger));
 app.use(cors({
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
